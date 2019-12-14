adef36498997f404297b0ce69520ac5cbf5f1be3
Divine Nature <divinehycenth@outlook.com>
2019-12-14 21:01:42 +0100

    changed host to use remote ip address

---
+++ build/precache-manifest.8c16f92a93d5f77c37b67cc99c00f643.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8813d937ee66779b4a1ab8cd58fff3c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8332f98c3c7cc3a52a68",
+    "url": "/static/css/main.95c3d2e1.chunk.css"
+  },
+  {
+    "revision": "7efc374211fb745c2f8b",
+    "url": "/static/js/2.b891cf4f.chunk.js"
+  },
+  {
+    "revision": "8332f98c3c7cc3a52a68",
+    "url": "/static/js/main.b6b3c387.chunk.js"
+  },
+  {
+    "revision": "2cdd201361aa99a2d6b4",
+    "url": "/static/js/runtime-main.f7d8d2ee.js"
+  }
+]);
