74c24d79bb2e0964bec7ece519781245cdc6505a
Divine Nature <divinehycenth@outlook.com>
2019-12-14 17:22:46 +0100

    created build folder

---
+++ build/precache-manifest.79ca5e19f498c31d207790a1bb49df13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba53e6f42610d855baa008f15337fe85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4544cabd970c7d9d902",
+    "url": "/static/css/main.95c3d2e1.chunk.css"
+  },
+  {
+    "revision": "7efc374211fb745c2f8b",
+    "url": "/static/js/2.b891cf4f.chunk.js"
+  },
+  {
+    "revision": "b4544cabd970c7d9d902",
+    "url": "/static/js/main.b83a0f7e.chunk.js"
+  },
+  {
+    "revision": "2cdd201361aa99a2d6b4",
+    "url": "/static/js/runtime-main.f7d8d2ee.js"
+  }
+]);
