8b7cf5e1e0f606d46003bf4e997d260b7761b300
Divine Nature <divinehycenth@outlook.com>
2019-12-14 21:00:16 +0100

    changed host to use remote ip address

--- superusers-real-estate-client/src/ApolloProvider.js
+++ superusers-real-estate-client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createUploadLink({
-  uri: 'http://localhost:5000/graphql'
+  uri: 'http://134.209.190.48:5000/graphql'
 });
 
 const authLink = setContext(() => {
