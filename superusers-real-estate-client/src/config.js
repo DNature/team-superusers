0e469a562216ff8a3e9a41c9db8f2eb7706a5b70
Divine Nature <divinehycenth@outlook.com>
2019-12-17 05:31:53 +0100

    corrected json file

--- superusers-real-estate-client/src/config.js
+++ superusers-real-estate-client/src/config.js
@@ -1,4 +1,4 @@
 const PRODUCTION = false;
 
 const domain = PRODUCTION ? '157.245.39.204' : 'localhost:8080';
-export const apiUrl = PRODUCTION ? `http://${domain}` : `http://${domain}`;
+export const apiUrl = !PRODUCTION ? `http://${domain}` : `http://${domain}`;
